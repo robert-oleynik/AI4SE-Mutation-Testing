5811b5f1ca7b750c12f9a0bb5d26d16872b3a2ce
Robert Oleynik <robert.oleynik@student.hpi.de>
2024-04-30 13:00:00 +0200

    feat: add Mutation annotation

---
+++ annotations/src/main/java/de/hpi/ai4se/mutations/Mutation.java
@@ -0,0 +1,10 @@
+package de.hpi.ai4se.mutations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
+public @interface Mutation {}
