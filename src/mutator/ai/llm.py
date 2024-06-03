d3fd5f9aa3bfbdb072ca0037f727ba72425c52e2
Marius Dörbandt <github.marius@doerbandt.de>
2024-06-03 23:45:03 +0200

    fix: remove <eos>

--- src/mutator/ai/llm.py
+++ src/mutator/ai/llm.py
@@ -45,9 +45,12 @@ class LLM:
         outputs = self.model.generate(**inputs, **kwargs)
 
         def decode(output):
-            result = transform(self.tokenizer.decode(output))
+            decoded = self.tokenizer.decode(output)
+            if decoded.endswith(self.tokenizer.eos_token):
+                return transform(decoded[:-len(self.tokenizer.eos_token)])
+            result = transform(decoded)
             if any(limiter.is_too_long(result) for limiter in limiters):
-                result = transform(self.tokenizer.decode(output[:-1]))
+                return transform(self.tokenizer.decode(output[:-1]))
             return result
 
         return [decode(output) for output in outputs]
