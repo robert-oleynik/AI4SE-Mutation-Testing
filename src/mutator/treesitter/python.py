8205c1be1f4bc692d632923a5ce9615c9fff109a
Marius Dörbandt <github.marius@doerbandt.de>
2024-06-24 17:15:22 +0200

    feat: add first_capture_named helper

--- src/mutator/treesitter/python.py
+++ src/mutator/treesitter/python.py
@@ -3,3 +3,11 @@ import tree_sitter_python as tsp
 
 tsLang = ts.Language(tsp.language())
 tsParser = ts.Parser(tsLang)
+
+
+def first_capture_named(capture_name: str, node: ts.Node, query: str):
+    for name, captures in tsLang.query(query).matches(node):
+        for capture, node in captures.items():
+            if capture == capture_name:
+                return node
+    return None
