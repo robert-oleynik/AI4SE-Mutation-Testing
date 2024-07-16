af872c5c956f3e9ab2a035969cebc026c0737a37
Marius Dörbandt <github.marius@doerbandt.de>
2024-07-16 13:00:18 +0200

    fix: use subprocess.run with timeout instead of polling

--- src/mutator/cli/test.py
+++ src/mutator/cli/test.py
@@ -1,6 +1,5 @@
 import pathlib
 import subprocess
-import time
 
 import click
 
@@ -8,7 +7,6 @@ from ..helper.timed import timed
 from ..result import Result
 from ..source import Filter
 from ..store import MutationStore
-from .spinner import Spinner
 
 
 @click.command(help="Runs the hole test suite for all mutations.")
@@ -70,7 +68,6 @@ def test(out_dir, project, filter, timeout, git_reset, test_dropped):
     result = Result()
 
     timeout *= 10
-    spinner = Spinner()
     for module_name, module in mutation.items():
         print("Testing Module:", module_name)
         for target_name, target in sorted(list(module.items()), key=lambda v: v[0]):
@@ -81,17 +78,17 @@ def test(out_dir, project, filter, timeout, git_reset, test_dropped):
             caught = 0
             count = len(target)
 
-            def status_update(icon: str, index: int, **kwargs):
+            def status_update(index: int, **kwargs):
                 missed = index - caught - syntax_error_count - timeout_count  # noqa: B023
                 print(
-                    f" {icon} {target_name:<80} [{index}/{count}]",  # noqa: B023
+                    f"{target_name:<80} [{index}/{count}]",  # noqa: B023
                     f"caught: {caught} missed: {missed}",  # noqa: B023
                     f"syntax errors: {syntax_error_count} timeout: {timeout_count}",  # noqa: B023
                     **kwargs,
                 )
 
             for i, (mutation, source) in enumerate(target):
-                status_update(spinner, i, end="\r")
+                status_update(i, end="\r")
                 if git_reset:
                     subprocess.run(
                         ["git", "reset", "--hard"], capture_output=True, cwd=project
@@ -105,24 +102,23 @@ def test(out_dir, project, filter, timeout, git_reset, test_dropped):
                     "-p",
                     mutation,
                 ]
-                process = subprocess.Popen(
-                    args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=project
-                )
-                counter = 0
-                while process.poll() is None and counter < timeout:
-                    time.sleep(0.1)
-                    spinner.next()
-                    status_update(spinner, i, end="\r")
-                    counter += 1
-                process.kill()
-                exit_code = process.poll()
-                is_timeout = counter >= timeout
+                try:
+                    is_timeout = False
+                    process = subprocess.run(
+                        args, capture_output=True, cwd=project, timeout=timeout
+                    )
+                    exit_code = process.returncode
+                    output = process.stdout.decode()
+                    output_err = process.stderr.decode()
+                except subprocess.TimeoutExpired:
+                    is_timeout = True
+                    exit_code = None
+                    output = "<timeout>"
+                    output_err = "<timeout>"
                 is_syntax_error = (
                     exit_code is not None and exit_code > 1 and not is_timeout
                 )
                 is_caught = exit_code != 0
-                output = process.stdout.read().decode()
-                output_err = process.stderr.read().decode()
                 output = output if output != "" else output_err
                 result.insert(
                     module_name,
@@ -141,5 +137,5 @@ def test(out_dir, project, filter, timeout, git_reset, test_dropped):
                     syntax_error_count += 1
                 if is_timeout:
                     timeout_count += 1
-            status_update("✔", count)
+            status_update(count)
     result.write(out_dir / "test-result.json")
