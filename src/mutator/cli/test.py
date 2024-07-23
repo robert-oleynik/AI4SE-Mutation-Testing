e3574c4c4547d0bc7c03c104879941efd00c19ea
Robert Oleynik <robert.oleynik@student.hpi.de>
2024-07-23 22:10:31 +0200

    feat: multiprocessing

--- src/mutator/cli/test.py
+++ src/mutator/cli/test.py
@@ -1,5 +1,9 @@
+import multiprocessing
+import os
 import pathlib
+import shutil
 import subprocess
+import tempfile
 
 import click
 
@@ -7,6 +11,69 @@ from ..helper.timed import timed
 from ..result import Result
 from ..source import Filter
 from ..store import MutantStore
+
+
+def _run_tester(x):
+    pid = os.getpid()
+    (
+        tmp_dir,
+        project_dir,
+        timeout,
+        git_reset,
+        module_name,
+        target_name,
+        i,
+        mutant,
+        source,
+    ) = x
+
+    project = f"{tmp_dir}/{pid}"
+    if not os.path.exists(project):
+
+        def ignore_outdir(dir, contents):
+            return ["out"]
+
+        shutil.copytree(project_dir, project, ignore=ignore_outdir)
+
+    if git_reset:
+        subprocess.run(["git", "reset", "--hard"], capture_output=True, cwd=project)
+
+    args = [
+        "python3",
+        "-m",
+        "mutator_runner",
+        "-m",
+        module_name,
+        "-p",
+        os.path.abspath(mutant),
+    ]
+    try:
+        is_timeout = False
+        process = subprocess.run(
+            args, capture_output=True, cwd=project, timeout=timeout
+        )
+        exit_code = process.returncode
+        output = process.stdout.decode()
+        output_err = process.stderr.decode()
+    except subprocess.TimeoutExpired:
+        is_timeout = True
+        exit_code = None
+        output = "<timeout>"
+        output_err = "<timeout>"
+
+    is_syntax_error = exit_code is not None and exit_code > 1 and not is_timeout
+    is_caught = exit_code != 0
+    output = output if output != "" else output_err
+    return (
+        module_name,
+        target_name,
+        mutant,
+        source,
+        is_caught,
+        is_syntax_error,
+        is_timeout,
+        output,
+    )
 
 
 @click.command(help="Runs the hole test suite for all mutants.")
@@ -50,8 +117,17 @@ from ..store import MutantStore
     show_default=True,
     help="Also run tests on dropped mutants. Used for testing purposes.",
 )
+@click.option(
+    "-j",
+    "--jobs",
+    type=int,
+    default=4,
+    help="Number of parallel jobs to execut in parallel",
+)
 @timed
-def test(out_dir, project, filter, timeout, git_reset, test_dropped):
+def test(out_dir, project, filter, timeout, git_reset, test_dropped, jobs):
+    tempdir = tempfile.mkdtemp(prefix="mutator-test")
+
     filters = Filter(filter)
 
     mutants = {}
@@ -67,74 +143,69 @@ def test(out_dir, project, filter, timeout, git_reset, test_dropped):
 
     result = Result()
 
-    for module_name, module in mutants.items():
-        print("Testing Module:", module_name)
-        for target_name, target in sorted(list(module.items()), key=lambda v: v[0]):
-            if not filters.match(module_name, target_name):
-                continue
-            timeout_count = 0
-            syntax_error_count = 0
-            caught = 0
-            count = len(target)
+    targets = [
+        (
+            tempdir,
+            project,
+            timeout,
+            git_reset,
+            module_name,
+            target_name,
+            i,
+            mutant,
+            source,
+        )
+        for module_name, module in mutants.items()
+        for target_name, target in sorted(list(module.items()), key=lambda v: v[0])
+        for i, (mutant, source) in enumerate(target)
+        if filters.match(module_name, target_name)
+    ]
+    timeout_count = 0
+    syntax_error_count = 0
+    caught = 0
+    count = len(targets)
 
-            def status_update(index: int, **kwargs):
-                missed = index - caught - syntax_error_count - timeout_count  # noqa: B023
-                print(
-                    f"{target_name:<80} [{index}/{count}]",  # noqa: B023
-                    f"caught: {caught} missed: {missed}",  # noqa: B023
-                    f"syntax errors: {syntax_error_count} timeout: {timeout_count}",  # noqa: B023
-                    **kwargs,
-                )
+    def status_update(name: str, index: int):
+        missed = index - caught - syntax_error_count - timeout_count  # noqa: B023
+        print(
+            f"{name:<80} [{index}/{count}]",  # noqa: B023
+            f"caught: {caught} missed: {missed}",  # noqa: B023
+            f"syntax errors: {syntax_error_count} timeout: {timeout_count}",  # noqa: B023
+            end="\r",
+        )
 
-            for i, (mutant, source) in enumerate(target):
-                status_update(i, end="\r")
-                if git_reset:
-                    subprocess.run(
-                        ["git", "reset", "--hard"], capture_output=True, cwd=project
-                    )
-                args = [
-                    "python3",
-                    "-m",
-                    "mutator_runner",
-                    "-m",
-                    module_name,
-                    "-p",
-                    mutant,
-                ]
-                try:
-                    is_timeout = False
-                    process = subprocess.run(
-                        args, capture_output=True, cwd=project, timeout=timeout
-                    )
-                    exit_code = process.returncode
-                    output = process.stdout.decode()
-                    output_err = process.stderr.decode()
-                except subprocess.TimeoutExpired:
-                    is_timeout = True
-                    exit_code = None
-                    output = "<timeout>"
-                    output_err = "<timeout>"
-                is_syntax_error = (
-                    exit_code is not None and exit_code > 1 and not is_timeout
-                )
-                is_caught = exit_code != 0
-                output = output if output != "" else output_err
-                result.insert(
-                    module_name,
-                    target_name,
-                    mutant.stem,
-                    mutant.absolute().relative_to(out_dir.resolve("./mutants")),
-                    source,
-                    is_caught,
-                    is_syntax_error,
-                    is_timeout,
-                    output,
-                )
-                if is_caught and not is_syntax_error and not is_timeout:
-                    caught += 1
-                if is_syntax_error:
-                    syntax_error_count += 1
-                if is_timeout:
-                    timeout_count += 1
-            status_update(count)
+    with multiprocessing.Pool(processes=jobs) as p:
+        i = 0
+        for x in p.imap(_run_tester, targets):
+            (
+                module_name,
+                target_name,
+                mutant,
+                source,
+                is_caught,
+                is_syntax_error,
+                is_timeout,
+                output,
+            ) = x
+            status_update(f"{module_name}:{target_name}", i)
+            result.insert(
+                module_name,
+                target_name,
+                mutant.stem,
+                mutant.absolute().relative_to(out_dir.resolve("./mutants")),
+                source,
+                is_caught,
+                is_syntax_error,
+                is_timeout,
+                output,
+            )
+            if is_caught and not is_syntax_error and not is_timeout:
+                caught += 1
+            if is_syntax_error:
+                syntax_error_count += 1
+            if is_timeout:
+                timeout_count += 1
+            i += 1
+    print()
     result.write(out_dir / "test-result.json")
+    shutil.rmtree(tempdir)
